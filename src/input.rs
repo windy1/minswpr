1185bee4421e8c62409a1846bec0ecb22c0a842c
Walker Crouse <walkercrouse@hotmail.com>
2020-02-25 22:54:15 -0500

    remove comment
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- src/input.rs
+++ src/input.rs
@@ -52,15 +52,3 @@ impl<'a> Execute for Input<ClickCell<'a>> {
         Ok(())
     }
 }
-
-// impl<'a> MouseUp for Input<ClickCell<'a>> {
-//     fn mouse_up(&mut self, x: i32, y: i32) -> Result<(), String> {
-//         println!("mouse_up = {:?}", Point::new(x, y));
-//         let meta = self.meta.as_ref().ok_or_else(|| "missing meta")?;
-//         match meta.board_render.get_cell_at(x, y) {
-//             Some(p) => meta.board.borrow_mut().reveal_from(p.x as u32, p.y as u32),
-//             None => {}
-//         }
-//         Ok(())
-//     }
-// }

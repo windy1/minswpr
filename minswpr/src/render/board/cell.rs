b85239adb97a4078438105b1751d671dd4a7056a
Walker Crouse <walkercrouse@hotmail.com>
2020-03-01 02:27:41 -0500

    remove comment
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- minswpr/src/render/board/cell.rs
+++ minswpr/src/render/board/cell.rs
@@ -17,7 +17,6 @@ pub(super) struct RenderCell<'a> {
 
 impl Render for RenderCell<'_> {
     fn render(&self, canvas: &mut WindowCanvas, pos: Point) -> Result<(), String> {
-        // let b = self.board.borrow();
         let cell = self.board.cell(self.board_pos.x, self.board_pos.y);
         let config = &self.config;
         let mines = &config.mines;
